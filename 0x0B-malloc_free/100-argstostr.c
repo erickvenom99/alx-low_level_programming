e3c40a5741fe67e1862f3bbc71dd29734902f3e4
erickvenom99 <prevenakhtar@mail.com>
2024-01-15 14:01:27 +0100

    update this code

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -29,7 +29,7 @@ char *argstostr(int ac, char **av)
 		}
 		total_length += arg_length;
 	}
-	arr = (char *)malloc((total_length + ac - 1) * sizeof(char));
+	arr = (char *)malloc((total_length + ac - 1) * sizeof(char) + 1);
 	if (arr == NULL)
 	{
 		return (NULL);
@@ -41,7 +41,10 @@ char *argstostr(int ac, char **av)
 		{
 			arr[curr_index++] = *arg++;
 		}
+		if (i < ac - 1)
+		{
 		arr[curr_index++] = '\n';
+		}
 	}
 	arr[curr_index] = '\0';
 	return (arr);
