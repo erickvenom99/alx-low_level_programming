849d7eee0042a370123b04dfce39de46bb981ebe
erickvenom99 <prevenakhtar@mail.com>
2024-01-24 21:49:50 +0100

    update code

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,12 +1,11 @@
-#ifndef __CALC_H__
-#define __CALC_H__
+#ifndef CALC_H
+#define CALC_H
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
-#endif
 /**
  * struct op - Struct op
  *
@@ -18,3 +17,4 @@ typedef struct op
 	char *op;
 	int (*f)(int a, int b);
 } op_t;
+#endif
