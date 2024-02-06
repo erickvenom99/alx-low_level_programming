0e57c62f8bad13bcd5860e948a50227f2971c361
erickvenom99 <prevenakhtar@mail.com>
2024-02-06 01:54:01 +0100

    update task 4

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -7,11 +7,12 @@
 
 void free_listint(listint_t *head)
 {
-	listint_t *current = head;
-	while (current != NULL)
+	listint_t *temp;
+
+	while (head)
 	{
-		listint_t *temp = current;
-		current = current->next;
-		free(temp);
+		temp = head->next;
+		free(head);
+		head = temp;
 	}
 }
