e38fca914e9bea1b5a01ae8405988757fcc042d1
erickvenom99 <prevenakhtar@mail.com>
2024-02-13 05:51:19 +0100

    update task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,13 +9,12 @@
  *
  * Return: 1 on success, -1 on failure, or 1 if
  */
-
 int create_file(const char *filename, char *text_content)
 {
 	int res;
 	int fd;
 	int len = 0;
-	int flags = O_CREAT | O_WRONLY | O_TRUNC;
+	int flags = O_WRONLY | O_CREAT | O_TRUNC;
 	mode_t mode = S_IRUSR | S_IWUSR;
 
 	if (filename == NULL)
@@ -25,21 +24,24 @@ int create_file(const char *filename, char *text_content)
 
 	if (fd == -1)
 		return (-1);
+
 	if (text_content == NULL)
 	{
 		close(fd);
 		return (1);
 	}
+
 	while (text_content[len] != '\0')
 		len++;
-	res = write(fd, text_content,  len);
+
+	res = write(fd, text_content, len);
 
 	if (res == -1 || res != len)
-		{
-			close(fd);
-			return (-1);
-		}
-
+	{
 		close(fd);
-		return (1);
+		return (-1);
+	}
+
+	close(fd);
+	return (1);
 }
